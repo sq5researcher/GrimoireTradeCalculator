b99043d9ff851e540565fe5cca4c352071a87c8e
sq5researcher <sq5researcher@gmail.com>
2026-06-07 08:35:22 +0900

    プロジェクトファイルを追加。

---
+++ GrimoireTradeCalculator/Form1.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace GrimoireTradeCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            comboBox1 = new ComboBox();
+            groupBox1 = new GroupBox();
+            numericUpDown10 = new NumericUpDown();
+            numericUpDown5 = new NumericUpDown();
+            label10 = new Label();
+            label5 = new Label();
+            numericUpDown9 = new NumericUpDown();
+            numericUpDown4 = new NumericUpDown();
+            label9 = new Label();
+            label4 = new Label();
+            numericUpDown8 = new NumericUpDown();
+            numericUpDown3 = new NumericUpDown();
+            label8 = new Label();
+            label3 = new Label();
+            numericUpDown7 = new NumericUpDown();
+            numericUpDown2 = new NumericUpDown();
+            label7 = new Label();
+            label2 = new Label();
+            numericUpDown6 = new NumericUpDown();
+            label6 = new Label();
+            numericUpDown1 = new NumericUpDown();
+            label1 = new Label();
+            label11 = new Label();
+            numericUpDown11 = new NumericUpDown();
+            button1 = new Button();
+            groupBox2 = new GroupBox();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown10).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown9).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown8).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown7).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown6).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown11).BeginInit();
+            SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "トレード", "リサイクル" });
+            comboBox1.Location = new Point(12, 12);
+            comboBox1.MaxDropDownItems = 2;
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(121, 23);
+            comboBox1.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(numericUpDown10);
+            groupBox1.Controls.Add(numericUpDown5);
+            groupBox1.Controls.Add(label10);
+            groupBox1.Controls.Add(label5);
+            groupBox1.Controls.Add(numericUpDown9);
+            groupBox1.Controls.Add(numericUpDown4);
+            groupBox1.Controls.Add(label9);
+            groupBox1.Controls.Add(label4);
+            groupBox1.Controls.Add(numericUpDown8);
+            groupBox1.Controls.Add(numericUpDown3);
+            groupBox1.Controls.Add(label8);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(numericUpDown7);
+            groupBox1.Controls.Add(numericUpDown2);
+            groupBox1.Controls.Add(label7);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(numericUpDown6);
+            groupBox1.Controls.Add(label6);
+            groupBox1.Controls.Add(numericUpDown1);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(12, 41);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(492, 100);
+            groupBox1.TabIndex = 1;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "所持グリモア数";
+            // 
+            // numericUpDown10
+            // 
+            numericUpDown10.Location = new Point(424, 49);
+            numericUpDown10.Maximum = new decimal(new int[] { 400, 0, 0, 0 });
+            numericUpDown10.Name = "numericUpDown10";
+            numericUpDown10.Size = new Size(47, 23);
+            numericUpDown10.TabIndex = 1;
+            // 
+            // numericUpDown5
+            // 
+            numericUpDown5.Location = new Point(424, 20);
+            numericUpDown5.Maximum = new decimal(new int[] { 400, 0, 0, 0 });
+            numericUpDown5.Name = "numericUpDown5";
+            numericUpDown5.Size = new Size(47, 23);
+            numericUpDown5.TabIndex = 1;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(390, 53);
+            label10.Name = "label10";
+            label10.Size = new Size(31, 15);
+            label10.TabIndex = 0;
+            label10.Text = "Lv10";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(393, 24);
+            label5.Name = "label5";
+            label5.Size = new Size(25, 15);
+            label5.TabIndex = 0;
+            label5.Text = "Lv5";
+            // 
+            // numericUpDown9
+            // 
+            numericUpDown9.Location = new Point(326, 49);
+            numericUpDown9.Maximum = new decimal(new int[] { 400, 0, 0, 0 });
+            numericUpDown9.Name = "numericUpDown9";
+            numericUpDown9.Size = new Size(47, 23);
+            numericUpDown9.TabIndex = 1;
+            // 
+            // numericUpDown4
+            // 
+            numericUpDown4.Location = new Point(326, 20);
+            numericUpDown4.Maximum = new decimal(new int[] { 400, 0, 0, 0 });
+            numericUpDown4.Name = "numericUpDown4";
+            numericUpDown4.Size = new Size(47, 23);
+            numericUpDown4.TabIndex = 1;
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(295, 53);
+            label9.Name = "label9";
+            label9.Size = new Size(25, 15);
+            label9.TabIndex = 0;
+            label9.Text = "Lv9";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(295, 24);
+            label4.Name = "label4";
+            label4.Size = new Size(25, 15);
+            label4.TabIndex = 0;
+            label4.Text = "Lv4";
+            // 
+            // numericUpDown8
+            // 
+            numericUpDown8.Location = new Point(227, 49);
+            numericUpDown8.Maximum = new decimal(new int[] { 400, 0, 0, 0 });
+            numericUpDown8.Name = "numericUpDown8";
+            numericUpDown8.Size = new Size(47, 23);
+            numericUpDown8.TabIndex = 1;
+            // 
+            // numericUpDown3
+            // 
+            numericUpDown3.Location = new Point(227, 20);
+            numericUpDown3.Maximum = new decimal(new int[] { 400, 0, 0, 0 });
+            numericUpDown3.Name = "numericUpDown3";
+            numericUpDown3.Size = new Size(47, 23);
+            numericUpDown3.TabIndex = 1;
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(196, 53);
+            label8.Name = "label8";
+            label8.Size = new Size(25, 15);
+            label8.TabIndex = 0;
+            label8.Text = "Lv8";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(196, 24);
+            label3.Name = "label3";
+            label3.Size = new Size(25, 15);
+            label3.TabIndex = 0;
+            label3.Text = "Lv3";
+            // 
+            // numericUpDown7
+            // 
+            numericUpDown7.Location = new Point(129, 49);
+            numericUpDown7.Maximum = new decimal(new int[] { 400, 0, 0, 0 });
+            numericUpDown7.Name = "numericUpDown7";
+            numericUpDown7.Size = new Size(47, 23);
+            numericUpDown7.TabIndex = 1;
+            // 
+            // numericUpDown2
+            // 
+            numericUpDown2.Location = new Point(129, 20);
+            numericUpDown2.Maximum = new decimal(new int[] { 400, 0, 0, 0 });
+            numericUpDown2.Name = "numericUpDown2";
+            numericUpDown2.Size = new Size(47, 23);
+            numericUpDown2.TabIndex = 1;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(98, 53);
+            label7.Name = "label7";
+            label7.Size = new Size(25, 15);
+            label7.TabIndex = 0;
+            label7.Text = "Lv7";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(98, 24);
+            label2.Name = "label2";
+            label2.Size = new Size(25, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Lv2";
+            // 
+            // numericUpDown6
+            // 
+            numericUpDown6.Location = new Point(37, 49);
+            numericUpDown6.Maximum = new decimal(new int[] { 400, 0, 0, 0 });
+            numericUpDown6.Name = "numericUpDown6";
+            numericUpDown6.Size = new Size(47, 23);
+            numericUpDown6.TabIndex = 1;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(6, 53);
+            label6.Name = "label6";
+            label6.Size = new Size(25, 15);
+            label6.TabIndex = 0;
+            label6.Text = "Lv6";
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(37, 21);
+            numericUpDown1.Maximum = new decimal(new int[] { 400, 0, 0, 0 });
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(47, 23);
+            numericUpDown1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(6, 24);
+            label1.Name = "label1";
+            label1.Size = new Size(25, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Lv1";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(18, 159);
+            label11.Name = "label11";
+            label11.Size = new Size(107, 15);
+            label11.TabIndex = 2;
+            label11.Text = "要求グリモアレベル：";
+            // 
+            // numericUpDown11
+            // 
+            numericUpDown11.Location = new Point(131, 155);
+            numericUpDown11.Maximum = new decimal(new int[] { 10, 0, 0, 0 });
+            numericUpDown11.Name = "numericUpDown11";
+            numericUpDown11.Size = new Size(41, 23);
+            numericUpDown11.TabIndex = 3;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(208, 155);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 4;
+            button1.Text = "計算";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Location = new Point(18, 194);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(486, 244);
+            groupBox2.TabIndex = 5;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "計算結果";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(523, 452);
+            Controls.Add(groupBox2);
+            Controls.Add(button1);
+            Controls.Add(numericUpDown11);
+            Controls.Add(label11);
+            Controls.Add(groupBox1);
+            Controls.Add(comboBox1);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown10).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown9).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown4).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown8).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown3).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown7).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown6).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown11).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private ComboBox comboBox1;
+        private GroupBox groupBox1;
+        private NumericUpDown numericUpDown1;
+        private Label label1;
+        private NumericUpDown numericUpDown10;
+        private NumericUpDown numericUpDown5;
+        private Label label10;
+        private Label label5;
+        private NumericUpDown numericUpDown9;
+        private NumericUpDown numericUpDown4;
+        private Label label9;
+        private Label label4;
+        private NumericUpDown numericUpDown8;
+        private NumericUpDown numericUpDown3;
+        private Label label8;
+        private Label label3;
+        private NumericUpDown numericUpDown7;
+        private NumericUpDown numericUpDown2;
+        private Label label7;
+        private Label label2;
+        private NumericUpDown numericUpDown6;
+        private Label label6;
+        private Label label11;
+        private NumericUpDown numericUpDown11;
+        private Button button1;
+        private GroupBox groupBox2;
+    }
+}
---
+++ GrimoireTradeCalculator/Form1.cs
@@ -0,0 +1,15 @@
+namespace GrimoireTradeCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+        }
+    }
+}
